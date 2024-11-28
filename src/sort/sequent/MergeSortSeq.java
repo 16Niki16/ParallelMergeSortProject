733adecde307359fa6dcb6a4b5553f1c5a2b4ed1
Nikolay Petrov <78559158+niki03petrov@users.noreply.github.com>
2024-11-28 17:01:09 +0200

    working parallel and sequential merge sort

--- src/sort/sequent/MergeSortSeq.java
+++ src/sort/sequent/MergeSortSeq.java
@@ -7,8 +7,9 @@ public class MergeSortSeq {
     public static void mergeSeq(int[] arr, int l, int r) {
         if (l < r) {
             int mid = l + (r - l) / Numbers.TWO;
+
             mergeSeq(arr, l, mid);
-            mergeSeq(arr, mid, r);
+            mergeSeq(arr, mid + Numbers.ONE, r);
 
             Helpers.merge(arr, l, mid, r);
         }
