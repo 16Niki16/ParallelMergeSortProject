d825dcf481158d6badd8f40852647b32917370ca
niki03petrov <78559158+niki03petrov@users.noreply.github.com>
2024-12-20 20:59:46 +0200

    format change

--- src/client/Client.java
+++ src/client/Client.java
@@ -28,6 +28,7 @@ public class Client implements ClientAPI {
             socketChannel.connect(new InetSocketAddress(SERVER_HOST, SERVER_PORT));
             while (true) {
                 System.out.println("""
+                                        
                                         Enter one of the commands:
                                          - Array with numbers divided by ','
                                          - Number of threads: <numbers of threads> Array: <Array with numbers divided by ','>
